74c2beea73eabdc9ef29bbd1c3f7ff6db9e3b772
siyakalra830 <114039216+siyakalra830@users.noreply.github.com>
2023-11-28 01:43:22 +0000

    generated user table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_28_014240) do
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.date "date_of_last_period"
+    t.integer "length_of_cycle"
+    t.string "workouts_enjoyed"
+    t.integer "workouts_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
